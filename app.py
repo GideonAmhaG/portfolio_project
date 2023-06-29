17ab3d58357a33a3ff2f7917d8d3ec7c092f8855
GideonAmhaG <gideonamha@gmail.com>
2023-06-29 13:07:10 +0300

    check

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from isolated_f import isolated
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'auyst$%gf'
 
 @app.route('/')
 def index():
@@ -15,10 +16,19 @@ def result():
     bc = float(request.form['BC'])
     fck = float(request.form['FCK'])
     fyk = float(request.form['FYK'])
+    bar = float(request.form['BAR'])
 
-    b, d, As, N, s = isolated(dl, ll, col, bc, fck, fyk)
+    """if not dl or not ll or not col or not bc or not fck or not fyk:
+        flash('An input field can not be blank.', category='error')
+    elif any(not isinstance(x, (int, float)) for x in (dl, ll, col, bc, fck, fyk)):
+        flash('An input field has to be a number.', category='error')
+    else:
+        flash('Calculation successful!', category='success')"""
+    
+    b, d, As, N, s = isolated(dl, ll, col, bc, fck, fyk, bar)
 
-    return render_template('index.html', b=b, d=d, As=As, N=N, s=s)
+    return render_template('index.html', b=b, d=d, As=As, N=N, s=s, bar=bar)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
